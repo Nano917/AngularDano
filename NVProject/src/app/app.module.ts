79f70bb900d684e13285236b3bfe4936cc5d9950
Nano <aaa@gmail.com>
2021-07-02 11:06:40 -0300

    creo componente header

--- NVProject/src/app/app.module.ts
+++ NVProject/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CategoriasComponent } from './components/categorias/categorias.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, CategoriasComponent],
+  declarations: [AppComponent, CategoriasComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
