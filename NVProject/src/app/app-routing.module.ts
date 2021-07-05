879a8d29c0c28b60ef44931c73b40fd251bf568e
Nano <aaa@gmail.com>
2021-07-05 14:51:19 -0300

    redirigir por nombre componente

--- NVProject/src/app/app-routing.module.ts
+++ NVProject/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { VehiculosComponent } from './components/vehiculos/vehiculos.component';
 
-const routes: Routes = [{ path: 'vehiculos', component: VehiculosComponent }];
+const routes: Routes = [{ path: 'Vehiculos', component: VehiculosComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
